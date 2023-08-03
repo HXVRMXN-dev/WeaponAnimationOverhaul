ea043d00672f498ebd3e77defd6a0a835dd3c998
HXVRMXN-dev <107282158+HXVRMXN-dev@users.noreply.github.com>
2023-08-04 00:01:16 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+authors {'HXVRMXN.dev', 'IcePick'}
+description 'IcePicks Animation Overhaul Mod made to work Server Sided by HXVRMXN.dev'
+version '1.0.0'
+
+files{
+     '*.ymt',
+	 '**/weaponanimations.meta'
+}
+
+data_file 'COMBAT_BEHAVIOUR_OVERRIDE_FILE' '*.ymt'
+data_file 'WEAPON_ANIMATIONS_FILE' '**/weaponanimations.meta'
