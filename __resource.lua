c7f176a4ee232b3a321971283ecfb643ddc1b22b
HXVRMXN-dev <107282158+HXVRMXN-dev@users.noreply.github.com>
2023-08-04 17:27:25 +0100

    Updated/Fixed

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'HXVRMXN.dev & IcePick'
+description 'IcePicks Animation Overhaul Mod made to work Server Sided by HXVRMXN.dev'
+version '1.0.0'
+
+files {
+	'weaponanimations.meta',
+	'**/weaponanimations.meta'
+}
+
+data_file 'WEAPON_ANIMATIONS_FILE' '**/weaponanimations.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta '
